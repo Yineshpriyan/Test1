9d4de1fc30fc72dd3ff5d9ebab825ab47f97f139
Yineshpriyan <iamyinesh97@gmail.com>
2023-07-23 14:00:13 +0530

    Welcome removed, b value changed to 15

--- yinesh.java
+++ yinesh.java
@@ -1,10 +1,9 @@
 public class yinesh{
     public static void main (String args[]){
-        int a=5, b=10;
+        int a=5, b=15;
         System.out.println((a+b));
-        System.out.println((a-b));
+        System.out.println((a*b));
         System.out.println("YINESH");
-        System.out.println("Welcome");       
-
+        
     }
 }
